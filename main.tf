22e94f645338f77d985752255d53c1b36cc489fb
i340771 <naresh.ganta@sap.com>
2025-10-22 13:23:43 +0530

    test

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "azurerm" {
-  features {
-    subscription_id = "1c8859e3-276f-40f1-afc9-1dd8f8dc18d7"
-  }
+  features {}
+  subscription_id = "1c8859e3-276f-40f1-afc9-1dd8f8dc18d7"
 }
 
 resource "azurerm_network_interface" "frontend" {
