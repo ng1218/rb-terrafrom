d2af219cc0cda6337797ba0dda0a7a9b3873b611
i340771 <naresh.ganta@sap.com>
2025-11-04 21:15:52 +0530

    test

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "resource_group" {
   rg_name   = "${each.key}-${var.env}"
 }
 
-module "applications" {
+/*module "applications" {
   depends_on                = [module.databases]
   for_each                  = var.applications
   source                    = "./modules/vm"
@@ -28,7 +28,7 @@ module "databases" {
   storage_image_reference   = var.storage_image_reference
   subnet_id                 = var.subnet_id
   zone_name                 = var.zone_name
-}
+}*/
 output "test" {
   value = module.resource_group.name
 }
