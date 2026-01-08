692f13586ad8948aa9055109e7ebea944437b3e9
i340771 <naresh.ganta@sap.com>
2026-01-08 10:33:37 +0530

    updating code

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -13,4 +13,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   identity {
     type = "SystemAssigned"
   }
+
+  aci_connector_linux {
+    subnet_name = "/subscriptions/ddffee8a-e239-4aa1-b7e0-b88ff5a2f9aa/resourceGroups/ngresources/providers/Microsoft.Network/virtualNetworks/vnet-ukwest/subnets/snet-ukwest-1"
+  }
 }
