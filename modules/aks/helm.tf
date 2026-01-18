447d8e2a9b0edfb2f8f4c2e208bb6aaaf12c02bf
i340771 <naresh.ganta@sap.com>
2026-01-18 20:23:16 +0530

    updating code

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -1,9 +1,18 @@
+resource null_resource "kubeconfig" {
+  provisioner "local-exec" {
+    command = "az aks get-credentials --name ${var.name} --resource-group ${var.rg_name}"
+  }
+}
+
 resource "helm_release" "external-secrets" {
+  depends_on = [
+    null_resource.kubeconfig
+  ]
   name       = "external-secrets"
   repository = "https://charts.external-secrets.io"
   chart      = "external-secrets"
-  namespace   = "roboshop-${var.env}"
-
+  namespace   = "devops"
+  create_namespace = true
   set = [
     {
       name  = "installCRDs"
